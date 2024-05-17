7a49f6f50acd85e2626dc39ba326b5c5b1b7c51c
AlphaJooj <vinixd476@gmail.com>
2024-05-17 09:56:38 -0300

    Otimização da página de orçamento
    Usando JS para detectar a bicicleta ou seguro selecionado, e marcar a opção na página de orçamento

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
+//Ativar links do menu
 const links = document.querySelectorAll(".header-menu a");
-
 function ativarLink(link) {
   const url = location.href;
   const href = link.href;
@@ -8,5 +8,18 @@ function ativarLink(link) {
     link.classList.add("ativo");
   }
 }
-
 links.forEach(ativarLink);
+
+//Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+  console.log(parametro);
+}
+
+parametros.forEach(ativarProduto);
